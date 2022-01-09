5b76a91e79c91035d4ced8713c9d85f4de8fe986
mohamedzaytoun <mohamed.hassan.1993y@gmail.com>
2022-01-10 00:38:14 +0200

    create point and line problems

---
+++ 05_graphic/point_location.py
@@ -0,0 +1,78 @@
+# Direction point accourding to line
+
+# Python3
+# program
+# to
+# determine
+# direction
+
+
+# of point from line segment
+
+# Structure for point in cartesian plane.
+class point:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+
+# Constant integers for directions
+RIGHT = 1
+LEFT = -1
+ZERO = 0
+PEOPLE_IDs={}
+
+def directionOfPoint(A, B, P):
+    global RIGHT, LEFT, ZERO
+
+    # Subtracting co-ordinates of
+    # point A from B and P, to
+    # make A as origin
+    B.x -= A.x
+    B.y -= A.y
+    P.x -= A.x
+    P.y -= A.y
+
+    # Determining cross Product
+    cross_product = B.x * P.y - B.y * P.x
+
+    # Return RIGHT if cross product is positive
+    if (cross_product > 0):
+        return RIGHT
+
+    # Return LEFT if cross product is negative
+    if (cross_product < 0):
+        return LEFT
+
+    # Return ZERO if cross product is zero
+    return ZERO
+
+def human_position(st_user_id,l_a,l_b,point):
+    people_id = {}
+
+    people_id.has_key(st_user_id)
+    return
+
+# Driver code
+if __name__ == "__main__":
+
+    A = point()
+    B = point()
+    P = point()
+
+    A.x = -30
+    A.y = 10  # A(-30, 10)
+    B.x = 29
+    B.y = -15  # B(29, -15)
+    P.x = 15
+    P.y = 28  # P(15, 28)
+
+    direction = directionOfPoint(A, B, P)
+
+    if (direction == 1):
+        print("Right Direction")
+    elif (direction == -1):
+        print("Left Direction")
+    else:
+        print("Point is on the Line")
